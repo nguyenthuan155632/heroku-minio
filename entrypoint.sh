5367045e5266392084a781b3a1681237c688c5d5
Nguyen Thuan <thuan.nv@raksul.com>
2020-07-21 17:59:13 +0700

    revert entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,4 @@ wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 export MINIO_ACCESS_KEY="$ACCESS_KEY"
 export MINIO_SECRET_KEY="$SECRET_KEY"
-mkdir data
-./minio server ./data
+./minio server --address :$PORT /mnt/minio/data
